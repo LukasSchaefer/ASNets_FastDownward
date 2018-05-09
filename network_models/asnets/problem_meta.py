c32db4325bc08563606ef24279cae3edf7cbfe30
Lukas Schaefer <luki.schaefer96@gmail.com>
2018-05-10 00:08:29 +0200

    changes in problem_meta adding computation of relations between ungrounded predicates and actions - minor changes to problem_meta now using whole task as input - added computation of relations between ungrounded predicates and actions (stored in dicts) - added sorting of relation-lists in values of dicts

--- network_models/asnets/problem_meta.py
+++ network_models/asnets/problem_meta.py
@@ -3,12 +3,10 @@ class ProblemMeta:
     and propositions in a pddl-task used for ASNets"""
 
     def __init__(self,
-                 name,
-                 actions,
-                 predicates,
+                 task,
                  propositional_actions,
                  grounded_predicates):
-        self.name = name
+        self.task = task
         self.propositional_actions = propositional_actions
         self.grounded_predicates = grounded_predicates
 
@@ -21,11 +19,12 @@ class ProblemMeta:
 
         # set dicts mapping from abstract actions/ predicates to grounding names
         self.action_name_to_prop_actions_names = \
-                self.__compute_act_to_prop_act_names_dict(actions, propositional_actions)
+                self.__compute_act_to_prop_act_names_dict(task.actions, propositional_actions)
         self.predicate_name_to_propositions_names = \
-                self.__compute_pred_to_grounding_names_dict(predicates, grounded_predicates) 
+                self.__compute_pred_to_grounding_names_dict(task.predicates, grounded_predicates) 
 
-        self.__compute_and_set_relations(propositional_actions, grounded_predicates)
+        self.__compute_and_set_relations_between_groundings(propositional_actions, grounded_predicates)
+        self.__compute_and_set_relations_between_abstracts(task.actions, task.predicates)
         
 
     def __compute_propositional_actions_by_name_dict(self, propositional_actions):
@@ -91,7 +90,7 @@ class ProblemMeta:
         return predicate_name_to_propositions
 
 
-    def __compute_and_set_relations(self, propositional_actions, grounded_predicates):
+    def __compute_and_set_relations_between_groundings(self, propositional_actions, grounded_predicates):
         """
         Computes relations among grounded propositional actions and predicates
         (action and prop are related iff prop appears in pre, add or delete list
@@ -120,10 +119,14 @@ class ProblemMeta:
         # of grounded actions with same underlying action schema have corresponding
         # propositions at the same index (necessary for weight sharing in ASNets)
         for propositional_action in propositional_actions:
-            self.__compute_and_set_relations_involving_action(propositional_action)
+            self.__compute_and_set_relations_involving_propositional_action(propositional_action)
+
+        # sort all action names in lists alphanumerically
+        for gr_pred in grounded_predicates:
+            self.prop_to_related_prop_action_names[gr_pred] = sorted(self.prop_to_related_prop_action_names[gr_pred])
 
 
-    def __compute_and_set_relations_involving_action(self, propositional_action):
+    def __compute_and_set_relations_involving_propositional_action(self, propositional_action):
         """
         Computes related grounded predicates (propositions) for grounded
         propositional action (= propositions that either appear in action's
@@ -146,9 +149,60 @@ class ProblemMeta:
                 related_propositions.append(proposition.__str__())
                 self.prop_to_related_prop_action_names[proposition].append(propositional_action.name)
 
-        self.prop_action_to_related_prop_names[propositional_action] = related_propositions
+        self.prop_action_to_related_prop_names[propositional_action] = sorted(related_propositions)
 
 
-    def get_grounded_predicates(self):
-        pass
-        # return self.grounded_predicates_by_name.
+    def __compute_and_set_relations_between_abstracts(self, actions, predicates):
+        """
+        Computes relations among abstract actions and predicates
+        (action and pred are related iff pred appears in pre, add or delete list
+        of action) and sets these in corresponding dicts in both directions
+        dicts:
+            - pred_to_related_action_names:
+                {pred: related actions.name with args}
+            - action_to_related_pred_names:
+                {action: related preds.name with pars}
+        :param actions: actions to use
+        :param predicates: predicates to use
+        """
+        # Dict of type action -> list(predicate)
+        self.action_to_related_pred_names = {}
+        # Dict of type predicate -> list(action)
+        self.pred_to_related_action_names = {}
+
+        # initialize dict entries for predicates all with empty list
+        # -> if no related actions than value is already correct and no in dict.keys()
+        # check necessary in self.__compute_and_set... function below
+        for pred in predicates:
+            self.pred_to_related_action_names[pred] = []
+
+        for action in actions:
+            self.__compute_and_set_relations_involving_action(action)
+
+        for pred in predicates:
+            self.pred_to_related_action_names[pred] = sorted(self.pred_to_related_action_names[pred])
+
+
+    def __compute_and_set_relations_involving_action(self, action):
+        """
+        Computes related predicates for action (= predicates that either appear
+        in action's preconditions, add- or delete-list) and sets these in
+        corresponding dicts in both directions
+        :param action: action to compute related predicates of
+        """
+        related_predicates = []
+        for part in action.precondition.parts:
+                if part.predicate not in related_predicates:
+                    related_predicates.append(part.predicate)
+                    predicate = self.task._predicate_dict()[part.predicate]
+                    if action.name not in self.pred_to_related_action_names[predicate]:
+                        self.pred_to_related_action_names[predicate].append(action.name)
+        for eff in action.effects:
+            pred = eff.literal.predicate
+            if pred not in related_predicates:
+                related_predicates.append(pred)
+                predicate = self.task._predicate_dict()[pred]
+                if action.name not in self.pred_to_related_action_names[predicate]:
+                    self.pred_to_related_action_names[predicate].append(action.name)
+
+        self.action_to_related_pred_names[action] = sorted(related_predicates)
