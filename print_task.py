bcfbf4d400dc5f86315f4db5168e22c2eb54e23e
Lukas Schaefer <luki.schaefer96@gmail.com>
2018-05-15 15:50:11 +0200

    Added print_task used to print different information for debugging/ testing purposes

---
+++ print_task.py
@@ -0,0 +1,98 @@
+import sys
+
+from src.translate.translator import main as translate
+from src.translate.normalize import normalize
+from src.translate.instantiate import instantiate, get_fluent_facts, get_fluent_predicates
+from src.translate.build_model import compute_model
+from src.translate.pddl_to_prolog import translate as pddl_to_prolog
+from src.translate.pddl.f_expression import *
+from src.translate.pddl.conditions import *
+
+sys.path.append("network_models/asnets")
+from problem_meta import ProblemMeta
+
+
+def print_grounded_predicates(task_meta):
+    print("Grounded Predicates:")
+    for prop in task_meta.grounded_predicates:
+        prop.dump()
+        print("ID: %d" % task_meta.grounded_predicate_name_to_id[prop.__str__()])
+        print("Related Actions: %d" % len(task_meta.gr_pred_to_related_prop_action_names[prop]))
+        for action in task_meta.gr_pred_to_related_prop_action_names[prop]:
+            print(action)
+        print("Related Action Ids: %s" % str(task_meta.get_related_propositional_action_ids(prop)))
+        print("")
+    print("\n")
+
+
+def print_propositional_actions(task_meta):
+    print("Propositional Actions:")
+    for action in task_meta.propositional_actions:
+        action.dump()
+        print("ID: %d" % task_meta.prop_action_name_to_id[action.name])
+        print(task_meta.prop_action_to_related_gr_pred_names[action])
+        print("Related Proposition Ids: %s" % str(task_meta.get_related_grounded_predicate_ids(action)))
+        print("")
+    print("\n")
+
+def print_predicates(task_meta):
+    print("Predicates:")
+    for pred in task_meta.task.predicates:
+        print(pred)
+        print(task_meta.pred_to_related_action_names[pred])
+        print("")
+    print("\n")
+
+
+def print_actions(task_meta):
+    print("Actions:")
+    for action in task_meta.task.actions:
+        print(action)
+        print(task_meta.action_to_related_pred_names[action])
+        print("")
+    print("\n")
+
+
+def print_grounded_predicates_by_id(task_meta):
+    grounded_predicate_name_id_list = []
+    for grounded_predicate_name, id in task_meta.grounded_predicate_name_to_id.items():
+        grounded_predicate_name_id_list.append((id, grounded_predicate_name))
+
+    for id, grounded_predicate_name in sorted(grounded_predicate_name_id_list):
+        print("%s: %s" % (id, grounded_predicate_name))
+
+
+def print_propositional_actions_by_id(task_meta):
+    prop_action_name_id_list = []
+    for prop_action_name, id in task_meta.prop_action_name_to_id.items():
+        prop_action_name_id_list.append((id, prop_action_name))
+
+    for id, prop_action_name in sorted(prop_action_name_id_list):
+        print("%s: %s" % (id, prop_action_name))
+
+
+def main(argv):
+    if argv is None or len(argv) != 3:
+        print("Please give two arguments!")
+        print("Usage: print_task <domain.pddl> <problem.pddl>")
+    else:
+        pddl_task, sas_task = translate([argv[1],argv[2]])
+        normalize(pddl_task)
+        prog = pddl_to_prolog(pddl_task)
+        model = compute_model(prog)
+        _, grounded_predicates, propositional_actions, _, _ = instantiate(pddl_task, model)
+        fluent_predicates = get_fluent_predicates(pddl_task, model)
+
+        pddl_task.simplify(fluent_predicates)
+        
+        task_meta = ProblemMeta(pddl_task, propositional_actions, grounded_predicates)
+
+        print_propositional_actions(task_meta)
+        print_grounded_predicates(task_meta)
+
+        print_predicates(task_meta)
+        # print_actions(task_meta)
+    
+
+if __name__ == "__main__":
+    main(sys.argv)
