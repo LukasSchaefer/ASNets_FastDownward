adapted utils broadcast and softmax for correct keras imports

network_models/asnets/utils.py
@@ -9,14 +9,16 @@ from keras import backend as K
 from keras.layers import Lambda
 
 def broadcast_to(pattern, array):
-    """Broacast ``array`` to match shape of ``pattern``."""
+    """
+    Broacast array to match shape of pattern.
+    """
     shape_layer = Lambda(lambda x: K.shape(x))
     pat_shape = shape_layer(pattern)
     arr_shape = shape_layer(array)
+
     floordiv_layer = Lambda(lambda x: tf.floordiv(x[0], x[1]))
     multiples = floordiv_layer([pat_shape, arr_shape])
-    multiples = Lambda(lambda x: K.cast(x, 'int32'), name='broadcast_int_cast_multiples')(multiples)
-    rv = Lambda(lambda x: K.tile(x[0], x[1]))([array, multiples])
+    rv = Lambda(lambda x: K.tile(x[0], K.cast(x[1], 'int32')))([array, multiples])
     return rv
 
 
@@ -31,12 +33,11 @@ def masked_softmax(activations, mask):
     :return: output tensor of masked softmax function representing the network's policy
         (ith value = probability to choose ith propositional action)
     """
-    mask_bool = Lambda(lambda x: K.cast(x, 'bool'), name='softmax_bool_cast_mask')(mask)
     min_acts = Lambda(lambda x: K.min(x, axis=-1, keepdims=True), name='softmax_min_acts')(activations)
     min_acts = broadcast_to(activations, min_acts)
-    disable_layer = Lambda(lambda x: tf.where(x[0], x[1], x[2]), name='softmax_disabled_to_min')
+    disable_layer = Lambda(lambda x: tf.where(K.cast(x[0], 'bool'), x[1], x[2]), name='softmax_disabled_to_min')
     # condition input left
-    disabled_min_out = disable_layer([mask_bool, activations, min_acts])
+    disabled_min_out = disable_layer([mask, activations, min_acts])
 
     # subtract out maximum for numeric stability
     max_acts = Lambda(lambda x: K.max(x, axis=-1, keepdims=True))(disabled_min_out)
@@ -51,7 +52,7 @@ def masked_softmax(activations, mask):
     any_valid = Lambda(lambda x: K.any(x, axis=-1, keepdims=True), name='softmax_any_valid')(mask)
     any_valid = broadcast_to(activations, any_valid)
 
-    safe_exps_layer = Lambda(lambda x: tf.where(x[0], x[1], x[2]), name='safe_exps')
+    safe_exps_layer = Lambda(lambda x: tf.where(K.cast(x[0], 'bool'), x[1], x[2]), name='safe_exps')
     # condition input left
     ones_like = Lambda(lambda x: K.ones_like(x), name='softmax_ones_like')(exps)
     safe_exps = safe_exps_layer([any_valid, exps, ones_like])