5ce99bf7a35750d19d83e1e9495cb3742ec114e4
Lukas Schaefer <luki.schaefer96@gmail.com>
2018-07-07 11:33:20 +0200

    minor formatting

--- src/search/search_engines/policy_search.cc
+++ src/search/search_engines/policy_search.cc
@@ -94,9 +94,9 @@ namespace policy_search {
         // preferences correspond to operator id by index
         assert(operator_ids.size() == operator_prefs.size());
 
-	// check if these are all applicable actions
-	vector<OperatorID> applicable_ops;
-	g_successor_generator->generate_applicable_ops(parent_state, applicable_ops);
+        // check if these are all applicable actions
+        vector<OperatorID> applicable_ops;
+        g_successor_generator->generate_applicable_ops(parent_state, applicable_ops);
 
         // find most probable/ preferenced operator
         int most_probable_op_index = -1;
@@ -117,9 +117,9 @@ namespace policy_search {
         OperatorID op_id = operator_ids[most_probable_op_index];
         OperatorProxy op_proxy  = task_proxy.get_operators()[op_id];
 
-	// reach new state
+        // reach new state
         GlobalState new_state = state_registry.get_successor_state(parent_state, op_proxy);
-	cout << "Policy reached state with id " << new_state.get_id() << " by applying op " << op_proxy.get_name() << endl;
+        cout << "Policy reached state with id " << new_state.get_id() << " by applying op " << op_proxy.get_name() << endl;
         SearchNode node = search_space.get_node(new_state);
         statistics.inc_generated();
 
